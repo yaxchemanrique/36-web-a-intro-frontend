9d76d7c32648340996d6af07021e5b7b910f6dbc
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-16 19:55:57 -0700

    feat: props

--- componentes/src/App.jsx
+++ componentes/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Saludo from "./components/Saludo.jsx"
+import Button from "./components/Button.jsx";
 
 function App() {
   return (
@@ -19,7 +20,24 @@ function App() {
       </header>
       <main>
         {/* <Saludo hola='este el el prop hola' adios='este es el prop adios'></Saludo> */}
-        <Saludo firstName='Yaxche'></Saludo>
+        <Button>
+          El texto entre mis dos etiquetas
+        </Button>
+
+        <Button>Iniciar sesion</Button>
+        <Button>
+          <code>Log in</code>
+          <span>Hola!</span>
+        </Button>
+
+        <Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, tempore dolore aliquid aspernatur natus eum ipsum sapiente autem. Beatae, molestiae deserunt. Omnis eligendi quos enim voluptates molestiae officiis amet ab
+        </Button>
+
+
+        
+        <Saludo></Saludo>
+        <Saludo firstName='Yaxche' lastName='Manrique'></Saludo>
         <Saludo firstName='Alejandro'/>
         <Saludo firstName='Aurelio'/>
         <Saludo firstName='Carlos'/>
--- componentes/src/components/Saludo.jsx
+++ componentes/src/components/Saludo.jsx
@@ -2,15 +2,30 @@ import React from "react";
 
 // rfce
 
-function Saludo(props) {
+/* function Saludo(props) {
   console.log(props)
   console.log(props.firstName)
+
+ const divStyles = {
+    background
+    ...
+  }
 
   return (
       <div style={{ background: "plum", marginBottom: "1rem" }}>
         Saludos, {props.firstName}
       </div>
     );
+} */
+
+function Saludo({ firstName = 'usuario', lastName }) {
+  // const {firstName, lastName} = props;
+
+  return (
+    <div style={{ background: "plum", marginBottom: "1rem" }}>
+      Saludos, {firstName} {lastName}
+    </div>
+  );
 }
 
 export default Saludo;
