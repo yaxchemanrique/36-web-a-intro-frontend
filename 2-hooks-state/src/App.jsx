434059a5799a153f18cf5e5cd17e2ef8d94a257c
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-21 20:25:52 -0700

    feat: contador con multiples botones

--- 2-hooks-state/src/App.jsx
+++ 2-hooks-state/src/App.jsx
@@ -2,22 +2,72 @@ import React from "react";
 
 function App() {
   //! React.useState() -> [variable, funcion] == [count, setCount] == [name, setName]
-  const initialCount = 10;
+  const initialCount = 50;
   const [count, setCount] = React.useState(initialCount);
 
-  function handleClick(increment) {
+  function handleClick(argumento) {
     // count = count + 1; * No se vale!
     // setCount(count + 1);
+    // setCount(count + increment);
+
+    switch (argumento) {
+      case "reset": {
+        const nextCount = initialCount;
+        setCount(nextCount);
+        window.alert(nextCount);
+        break;
+      }
+      case "random": {
+        let nextCount = Math.floor(Math.random() * 101);
+        setCount(nextCount);
+        window.alert(nextCount);
+        break;
+      }
+      default:
+        const nextCount = count + argumento;
+        setCount(nextCount);
+        window.alert(nextCount);
+    }
+  }
+  /*
+  function handleClick(increment){
+    // count ++;
     setCount(count + increment);
   }
+
+  function handleClickdecreas(decremento){
+    // count ++;
+    if((count-decremento) <= 0 ){
+      setCount(0);
+    }else{
+      setCount(count-decremento);
+    }
+  }
+  function handleClickreset(){
+    setCount(initialCount);
+  }
+  function handleClickrRandom(min, max){
+    setCount(Math.floor(Math.random() * (max - min + 1)) + min);
+  }*/
 
   return (
     <React.Fragment>
       <></>
       <div>{count}</div>
-      <br/>
-      <button onClick={() => handleClick(1)} >Aumenta en 1</button>
-      <button onClick={() => handleClick(10)} >Aumenta en 10</button>
+      <br />
+      {/* <button onClick={() => handleClick(1)}>Aumenta en 1</button>
+      <button onClick={() => handleClick(10)}>Aumenta en 10</button>
+      <button>Reiniciar</button>
+      <button>Numero random (0-100)</button>
+      <button>Disminuye en 10</button>
+      <button>Disminuye en 1</button> */}
+
+      <button onClick={() => handleClick(1)}>Aumenta en 1</button>
+      <button onClick={() => handleClick(10)}>Aumenta en 10</button>
+      <button onClick={() => handleClick("reset")}>Reiniciar</button>
+      <button onClick={() => handleClick("random")}>Random</button>
+      <button onClick={() => handleClick(-1)}>Disminuye en 1</button>
+      <button onClick={() => handleClick(-10)}>Disminuye en 10</button>
       {/* <button onClick={()=>...} >Aumenta en 1</button> */}
     </React.Fragment>
   );
@@ -29,4 +79,4 @@ export default App;
   handleClick(1)
 } */
 
-
+//! Revisamos en 10 min
